c82d412b126d72261500ada4223e892ebe8d8f01
aschoettler <sandy.schoettler@gmail.com>
2020-02-23 21:54:25 -0800

    M2

---
+++ math/m2/run-m2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ $(docker start M2) ]]; then
+    docker logs --since 2s M2
+    docker attach M2
+else
+    echo ""
+    echo "Getting Macaulay2"
+    docker run -it --name=M2 -v ~/M2:/home/m2user mikestillman/test-macaulay2-1.8.2-0 M2;
+    exit
+fi
